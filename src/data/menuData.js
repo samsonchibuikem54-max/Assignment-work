8801aaf4cd5b778acdf8a5bd463d51b3fbd1a322
chibuikemk2 <samsonchibuikem3@gmail.com>
2026-04-23 07:58:55 +0100

    Fix image imports for deployment

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -1,47 +1,53 @@
+import food1 from "../assets/Foods/download1.png";
+import food2 from "../assets/Foods/download.png";
+import food3 from "../assets/Foods/images.png";
+import food4 from "../assets/Foods/images2.png";
+import food5 from "../assets/Foods/images1.png";
+import food6 from "../assets/Foods/images5.png";
+
 const menuData = [
   {
     id: 1,
     name: "Chicken Biryani",
     price: 22.99,
     category: "Main",
-    image: "src/assets/Foods/download (1).png",
+    image: food1,
   },
   {
     id: 2,
     name: "Chicken Tikka Masala",
     price: 19.99,
     category: "Starter",
-    image: "src/assets/Foods/download.png",
+    image: food2,
   },
   {
     id: 3,
     name: "Vegetable Biryani",
     price: 14.99,
     category: "Dessert",
-    image: "src/assets/Foods/images.png",
+    image: food3,
   },
   {
     id: 4,
     name: "Butter Naan",
     price: 3.99,
     category: "Main",
-    image: "src/assets/Foods/images2.png",
+    image: food4,
   },
   {
     id: 5,
     name: "Palak Paneer",
     price: 16.99,
     category: "Main",
-    image: "src/assets/Foods/images 1.png",
+    image: food5,
   },
   {
     id: 6,
     name: "Roti with Paneer Gravy",
     price: 17.99,
     category: "Main",
-    image: "src/assets/Foods/images 5.png",
+    image: food6,
   },
- 
 ];
 
 export default menuData;
